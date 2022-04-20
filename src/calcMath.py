c5419197173051dcd108890cf2aeed13bdf0f760
Samuel Simun <samuelsimun@gmail.com>
2022-04-20 11:16:42 +0200

    Added root funtion into math library

--- src/calcMath.py
+++ src/calcMath.py
@@ -1,39 +1,46 @@
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
     return a - b
+
 
 def mul(a, b):
     return a * b
 
+
 def div(a, b):
     if b == 0:
         return "DIVISION BY ZERO"
-    return a/b
+    return a / b
+
 
 def factorial(x):
-    pom=0
+    pom = 0
     for i in range(1, x):
         pom = pom * i
 
     return pom
 
+
 def power(x, n):
-    if (n < 0):
+    if n < 0:
         return "NOT NUTURAL NUMBER"
 
-    pom=0
+    pom = 0
     for i in range(1, n):
-        pom=pom*x
+        pom = pom * x
 
     return x
 
+
 def root(n, x):
-    """Vrací ntou odmocninu z x."""
-    #TODO
-    return 0
+    if x < 0:
+        return "NUMBER IS NOT POSITIVE NUMBER"
+
+    return x ** (1/n)
+
 
 def mod(a, b):
-
     return a % b

bdda3f6fba4a35ff31afdf056ff5ea467ea5b64a
marekoz <marek.kozumplik3@gmail.com>
2022-04-28 00:41:30 +0200

    Update standardDeviation.py
    Added cProfile

--- src/standardDeviation.py
+++ src/standardDeviation.py
@@ -1,6 +1,6 @@
 from calcMath import *
 from typing import List
-
+import cProfile
 
 def standard_deviation(numbers_list: List[int]) -> float:
     n: int = len(numbers_list)
@@ -12,7 +12,15 @@ def standard_deviation(numbers_list: List[int]) -> float:
 
 
 if __name__ == '__main__':
-    input_str: str = input("Vlozte cisla oddelene carkou: ")
+    prof = cProfile.Profile()
+    prof.enable()  # profiling start
+
+    input_str: str = input()
     numbers_string: List[str] = input_str.split()
     numbers: List[int] = [int(i) for i in numbers_string]
     print(standard_deviation(numbers))
+
+    prof.disable()  # profiling end
+    prof.print_stats(sort='calls')  # vypise profiling na stdout
+    prof.dump_stats("profiling_out.prof")  # vygeneruje prof soubor napr. pro snakeviz
+
